dfd24957733d8fa997d8c1b3871eecde39eed454
siva99163 <manjulasivasankar99@gmail.com>
2026-02-06 23:25:40 -0800

    Update app.java

--- app.java
+++ app.java
@@ -1,5 +1,10 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
